eba8a4f2c7f4b05db7f6f8f5d4ab26051f4b4b65
a.anikin <79037128326@ya.ru>
2021-10-18 02:53:45 +0300

    ready

--- index.js
+++ index.js
@@ -1,44 +1,17 @@
-const frontMatter = require('front-matter');
-const handlebars = require('handlebars');
-const handlebarsWax = require('handlebars-wax');
-const handlebarsLayouts = require('handlebars-layouts');
-const handlebarsHelpersPackage = require('handlebars-helpers');
+const Handlebars = require('handlebars');
 const { Transformer } = require('@parcel/plugin');
-
-const handlebarsHelpers = handlebarsHelpersPackage();
-const { loadUserConfig, parseSimpleLayout } = require('./utils');
-
-const userConfig = loadUserConfig();
-const config = Object.assign({}, {
-    data: 'src/markup/data',
-    decorators: 'src/markup/decorators',
-    helpers: 'src/markup/helpers',
-    layouts: 'src/markup/layouts',
-    partials: 'src/markup/partials',
-}, userConfig);
-
-
-const wax = handlebarsWax(handlebars)
-    .helpers(handlebarsLayouts)
-    .helpers(handlebarsHelpers)
-    .helpers(`${config.helpers}/**/*.js`)
-    .data(`${config.data}/**/*.{json,js}`)
-    .decorators(`${config.decorators}/**/*.js`)
-    .partials(`${config.layouts}/**/*.{hbs,handlebars,js}`)
-    .partials(`${config.partials}/**/*.{hbs,handlebars,js}`);
 
 const transformer = new Transformer({
     async transform({asset}) {
-
-        const code = await asset.getCode();
-        const frontmatter = frontMatter(code);
-        const content = parseSimpleLayout(frontmatter.body, config);
-        const data = Object.assign({}, frontmatter.attributes, { NODE_ENV: process.env.NODE_ENV });
-        const html = wax.compile(content)(data);
-
-        asset.type = 'html';
-        asset.setCode(html);
-
+        const source = await asset.getCode();
+        const precompiled = Handlebars.precompile(source);
+        const js = `
+                import Handlebars from 'handlebars/dist/handlebars.runtime';
+                const templateFunction = Handlebars.template(${precompiled});
+                export default templateFunction;
+                `;
+        asset.type = 'js';
+        asset.setCode(js);
         return [asset];
     }
 });
